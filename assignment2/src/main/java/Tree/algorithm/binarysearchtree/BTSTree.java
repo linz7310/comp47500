0888b7c27ed164888cc677e695e53d6b156df042
yilin <linz0731@outlook.com>
2024-03-18 01:24:59 +0000

    Initial commit

---
+++ assignment2/src/main/java/Tree/algorithm/binarysearchtree/BTSTree.java
@@ -0,0 +1,78 @@
+package Tree.algorithm.binarysearchtree;
+
+import Tree.datastructure.binarysearchtree.TreeNode;
+import java.util.LinkedList;
+
+/**
+ * ClassName BTSTree
+ * Package java.algorithm.binarysearchtree
+ * Description:
+ *
+ * @Author: Lin
+ * @Creat: 2024/3/18
+ */
+public class BTSTree {
+    public boolean isValidBST(TreeNode node) {
+        TreeNode p = node;
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        long prev = Long.MIN_VALUE; // 代表上一个值
+        while (p != null || !stack.isEmpty()) {
+            if (p != null) {
+                stack.push(p);
+                p = p.left;
+            } else {
+                TreeNode pop = stack.pop();
+                // 处理值
+//                System.out.println("compare:" + prev + " " + pop.val);
+                if (prev >= pop.val) {
+                    return false;
+                }
+                prev = pop.val;
+                p = pop.right;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        /*
+                4
+               / \
+              2   6
+             / \
+            1   3
+        */
+        TreeNode root1 = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6));
+        System.out.println(new BTSTree().isValidBST(root1));
+//        System.out.println("---------------");
+        /*
+                4
+               / \
+              2   6
+                 / \
+                3   7
+         */
+        TreeNode root2 = new TreeNode(4, new TreeNode(2), new TreeNode(6, new TreeNode(3), new TreeNode(7)));
+        System.out.println(new BTSTree().isValidBST(root2));
+//        System.out.println("---------------");
+
+        /*
+               1
+              /
+             1
+         */
+        TreeNode root3 = new TreeNode(1, new TreeNode(1), null);
+        System.out.println(new BTSTree().isValidBST(root3));
+//        System.out.println("---------------");
+
+        /*
+              3
+               \
+                4
+               /
+              5
+         */
+        TreeNode root4 = new TreeNode(3, null, new TreeNode(4, new TreeNode(5), null));
+        System.out.println(new BTSTree().isValidBST(root4));
+    }
+}
---
+++ assignment2/src/main/java/Tree/datastructure/binarysearchtree/TreeNode.java
@@ -0,0 +1,25 @@
+package Tree.datastructure.binarysearchtree;
+
+/**
+ * 力扣用到的二叉搜索树节点
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(val);
+    }
+}
